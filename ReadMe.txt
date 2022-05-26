ab26ce13dce58a91e3b29d617db6b2f48df52b2f
FraserCarpenter <fraserac3@gmail.com>
2022-05-26 15:40:27 +0200

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+Publish URL: 
+    - Project: https://expo.dev/@dzonepanther/react-native-crypto?serviceType=classic&distribution=expo-go
---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RektButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RektButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
